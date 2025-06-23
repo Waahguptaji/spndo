f3d17bcf81c7691b04f4f0b90db41bd729c43810
waahguptaji <sahilgupta742@gmail.com>
2025-06-24 00:37:16 +0530

    chore: initialize Spring Boot backend with MySQL

---
+++ server/src/main/java/com/spndo/server/SpndoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.spndo.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpndoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpndoBackendApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/spndo/server/SpndoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spndo.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpndoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
