2dfc897a91ff40c04b2d5ad95c041b1dadf17fdc
waahguptaji <sahilgupta742@gmail.com>
2025-08-22 00:18:17 +0530

    feat(goals): create goals and addGoal page [#36]

---
+++ client/src/app/(app)/goals/page.tsx
@@ -0,0 +1,12 @@
+import GoalsWidget from "@/components/dashboard/GoalsWidget";
+import React from "react";
+
+const GoalsPage = () => {
+  return (
+    <div>
+      <GoalsWidget />
+    </div>
+  );
+};
+
+export default GoalsPage;
