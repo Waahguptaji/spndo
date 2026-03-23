7301936579c80ac2ca99f070c37c7a89210d85a2
Asmita Sagarkar <asmitasagarkar@Asmitas-MacBook-Air.local>
2026-03-24 00:06:24 +0530

    feat/fe-backend-integration : integrate frontend and backend for login. [#112]

--- client/src/lib/api/auth.ts
+++ client/src/lib/api/auth.ts
@@ -1,12 +1,12 @@
 import apiFetch from "../apiClient";
 
-type RegisterInput = {
+export type RegisterInput = {
   email: string;
   password: string;
   confirmPassword: string;
 };
 
-type RegisterResponse = {
+export type RegisterResponse = {
   message: string;
   accessToken: string;
   refreshToken: string;
@@ -21,11 +21,55 @@ type RegisterResponse = {
   };
 };
 
-const registerUser = async (data: RegisterInput): Promise<RegisterResponse> => {
+export type LoginInput = {
+  email : string;
+  password : string;
+}
+export type LoginResponse = {
+  message: string;
+  accessToken: string;
+  refreshToken: string;
+  user: {
+    id: string;
+    email: string;
+    phone: string | null;
+    profile_data: JSON | null;
+    is_active: boolean;
+    created_at: string;
+    updated_at: string;
+  };
+
+  
+}
+
+
+export const registerUser = async (data: RegisterInput): Promise<RegisterResponse> => {
   return apiFetch("/auth/register", {
     method: "POST",
     body: data,
   });
 };
 
-export { registerUser, logoutUser, isLoggedIn };
+
+
+export const login = async (data: LoginInput): Promise<LoginResponse> => {
+    return apiFetch("/auth/login", {
+        method: "POST",
+        body: data
+    })
+}
+export function getGoals(){
+    return apiFetch("/goals");
+}
+export function getUserProfile(){
+    return apiFetch("/user/me",{
+        method: "GET"
+    });
+}
+
+export function updateUserProfile(data: JSON){
+    return apiFetch("/user/me",{
+        method:"PATCH",
+        body:data
+    })
+}
